5477e5512467b31be670d50fb61b9d342f919ceb
nymann <s144833@student.dtu.dk>
2015-06-30 02:18:47 +0200

    Problem 1 committed, for the sake of completion.

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,21 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by NEX on 30/06/2015.
  * Re-did problem 1 to 4 for the sake of completion
+ *
+ * Observation i did during testing, was that when sumOfMultiplesBelowThisNumber, where equal to:
+ * 100, result = 2318
+ * 1000, result = 233168
+ * 10000, result = 23331668
+ * 100000, result = 2333316668
+ * 1000000, result = 233333166668
+ * 10000000, result = 23333331666668
+ * 100000000, result = 2333333316666668
+ *
+ * and so on.. a rather interesting pattern, not sure yet why this is.
  */
 public class Problem1 {
 
-    int sumOfMultiplesBelowThisNumber = 1000;
+    long sumOfMultiplesBelowThisNumber = 100000000;
 
     public Problem1() {
         sumOfAllMultiplesOf3Or5(sumOfMultiplesBelowThisNumber);
     }
 
-    void sumOfAllMultiplesOf3Or5(int belowThis) {
-        int i = 1;
-        int sum = 0;
+    void sumOfAllMultiplesOf3Or5(long belowThis) {
+        long i = 1;
+        long sum = 0;
 
         while (i < belowThis) {
             if (i * 3 < belowThis) {
